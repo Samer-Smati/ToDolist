771ec88d1de2574bfeafe15349b1dcaa4f8db512
Samer-Smati <samersmati@gmail.com>
2022-06-08 09:26:07 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavBar from './components/ui/NavBar';
+import MainPage from './components/MainPage'
+import { useState } from 'react';
+function App() {
+  const [search,setSearch] = useState('');
+
+  return (
+    <div className="App">
+      <NavBar setSearch={setSearch} />
+      <MainPage search={search}/>
+    </div>
+  );
+}
+
+export default App;
